811c3c90dbb311349c7578acea2e606f6d121a13
cho881020 <kj_cho@idealidea.co.kr>
2019-12-14 14:14:52 +0900

    Initial commit

---
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.tjeit.a20191214_02_baseballgame
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
