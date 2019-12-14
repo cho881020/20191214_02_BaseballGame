09d6211c86a6eff48470f7677d467608a3d8b8c9
cho881020 <kj_cho@idealidea.co.kr>
2019-12-14 14:43:59 +0900

    컴퓨터 출제 숫자 배열 추가

--- app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : BaseActivity() {
 
+    val computerNumArray = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
