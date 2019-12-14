af7563e4450881aa71c8765e76d4e0d4bace7596
csh0117 <tnsgur!053>
2019-12-14 15:10:36 +0900

    컴퓨터의 문제 출제 기능

--- app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.tjeit.a20191214_02_baseballgame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BaseActivity() {
 
@@ -18,11 +18,36 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-
+        makeComputerNumberes()
     }
 
     override fun setValues() {
+    }
 
+    fun makeComputerNumberes() {
+        for (i in 0..2) {
+            Log.d("log]for문","${i}")
+            while (true) {
+                val randomNum = (Math.random() * 9 + 1).toInt()   // 0 <= 랜덤값 * 9 + 1 < 9
+
+                var isDuplicateOk = true
+
+                for (num in computerNumArray) {
+                    if (num == randomNum) {
+                        isDuplicateOk = false
+                        break
+                    }
+                }
+
+                if (isDuplicateOk) {
+                    computerNumArray.add(randomNum)
+                    break
+                }
+            }
+        }
+        for (num in computerNumArray) {
+            Log.d("log]출제된 숫자","${num}")
+        }
     }
 
 }
