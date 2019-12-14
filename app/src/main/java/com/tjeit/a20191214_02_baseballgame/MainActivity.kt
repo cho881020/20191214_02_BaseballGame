e038c8ec7eca251c35c06306b9327ecabf18eb3f
kvoyant <kvoyant@gmail.com>
2019-12-14 15:11:48 +0900

    코드 작성

--- app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.tjeit.a20191214_02_baseballgame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BaseActivity() {
 
@@ -23,6 +23,36 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+    }
+
+    fun makeComputerNumbers() {
+        for ( i in 0..2) {
+            Log.d("for문","${i}")
+
+            while(true) {
+//                val randomNum = Math.random() // 1 <= 랜덤값 < 1
+                val randomNum = (Math.random() * 9 + 1).toInt() // 1 <= 랜덤값 * 9 + 1 < 10
+
+                var isDuplicateOk = true
+
+                for(num in computerNumArray) {
+                    if(num == randomNum) {
+                        isDuplicateOk = false
+                        break
+                    }
+                }
+
+                if(isDuplicateOk) {
+                    computerNumArray.add(randomNum)
+                    break
+                }
+
+            }
+        }
+
+        for(num in computerNumArray) {
+            Log.d("출제된 숫자", "${num}")
+        }
     }
 
 }
