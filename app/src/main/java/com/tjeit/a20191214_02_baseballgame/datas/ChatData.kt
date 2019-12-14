cff22ccd297c99ac0ae61b8c23882b6e04a4a9b2
soojung <blessme333@gmail.com>
2019-12-14 15:19:02 +0900

    채팅 데이터 / 한 줄 짜리 XML 그리기

---
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/datas/ChatData.kt
@@ -0,0 +1,10 @@
+package com.tjeit.a20191214_02_baseballgame.datas
+
+class ChatData(content:String, writer:String) {
+
+    var content = content
+    var writer = writer
+
+//    Ex. 사용자 - 567 입력 : ChatData ("567", "user")
+
+}
