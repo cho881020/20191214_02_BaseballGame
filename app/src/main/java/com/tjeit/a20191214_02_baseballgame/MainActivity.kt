aa9f62e9acb7be4fff9e9696e1c169d828f49fa7
csh0117 <tnsgur!053>
2019-12-14 16:37:50 +0900

    채팅 입력 시 맨 밑으로 부드럽게 자동 스크롤 되도록

--- app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
@@ -91,9 +91,11 @@ class MainActivity : BaseActivity() {
         }
         chatDataList.add(ChatData("computer: ${strikeCount}S ${ballCount}B입니다.","computer"))
         chatAdapter?.notifyDataSetChanged()
+        chatListView.smoothScrollToPosition(chatDataList.size-1)   // 10칸 : 마지막 칸 9번
 
         if (strikeCount == 3) {
             chatDataList.add(ChatData("computer: 축하합니다! 정답입니다.", "computer"))
+            chatListView.smoothScrollToPosition(chatDataList.size-1)   // 10칸 : 마지막 칸 9번
         }
     }
 
