d9dacaa36db5432e3defc1193381e80774c28a28
nohhyejin813 <nohhyejin813@gmai.com>
2019-12-14 15:19:28 +0900

    채팅데이터와 한줄짜리 XML그리기

---
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/datas/ChatData.kt
@@ -0,0 +1,10 @@
+package com.tjeit.a20191214_02_baseballgame.datas
+
+class ChatData(content:String, writer:String) {
+
+    var content = content
+    var writer = writer
+
+    // ex.사용자가 567 입력 : ChatData("567", "user")
+
+}
