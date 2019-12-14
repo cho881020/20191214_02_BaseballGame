35d48418d21fe492f2dfcd3ca73dc37ed7c82e04
jun <vagrantsj@gmail.com>
2019-12-14 15:10:40 +0900

    컴퓨터의 문제 출제 기능

--- app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
@@ -2,10 +2,11 @@ package com.tjeit.a20191214_02_baseballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BaseActivity() {
 
-//    문제로 나온 숫자를 담기 위한 배열
+    //    문제로 나온 숫자를 담기 위한 배열
     val computerNumArray = ArrayList<Int>()
 
 //    계석준의 전용 브랜치 추가
@@ -23,6 +24,34 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+    }
+
+    fun makeComputerNumbers() {
+        for (i in 0..2) {
+            Log.d("for문", "${i}")
+
+            while (true) {
+                val randomNum = (Math.random() * 9 + 1).toInt() // 0<= 랜덤값 < 1
+                var isDuplicateOk = true
+
+                for (num in computerNumArray) {
+                    if (num == randomNum) {
+                        isDuplicateOk = false
+                        break
+                    }
+
+                }
+
+                if (isDuplicateOk) {
+                    computerNumArray.add(randomNum)
+                    break
+                }
+            }
+        }//for end
+
+        for (num in computerNumArray){
+            Log.d("출제된 숫자","${num}")
+        }
     }
 
 }
