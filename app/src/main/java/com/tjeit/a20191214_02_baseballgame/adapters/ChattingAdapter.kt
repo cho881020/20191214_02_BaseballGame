d6dc4232c84deb6fa51c3a1b11dd7bbe3c06d628
kvoyant <kvoyant@gmail.com>
2019-12-14 15:37:39 +0900

    채팅 어댑터 초안 작성

---
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/adapters/ChattingAdapter.kt
@@ -0,0 +1,31 @@
+package com.tjeit.a20191214_02_baseballgame.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tjeit.a20191214_02_baseballgame.R
+import com.tjeit.a20191214_02_baseballgame.datas.ChatData
+
+class ChattingAdapter(context:Context, resId:Int, list:ArrayList<ChatData>) : ArrayAdapter<ChatData>(context, resId, list) {
+
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        tempRow?.let {
+
+        }.let {
+            tempRow = inf.inflate(R.layout.chat_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+}
