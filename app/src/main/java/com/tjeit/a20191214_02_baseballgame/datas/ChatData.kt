a3d74a41b4f6a1a7f7002d4197d901068a3a6021
hyun <rlaalgus132@naver.com>
2019-12-14 15:19:06 +0900

    채팅 데이터 / 한줄짜리 xml 그리기

---
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/datas/ChatData.kt
@@ -0,0 +1,8 @@
+package com.tjeit.a20191214_02_baseballgame.datas
+
+class ChatData(content:String, writer:String) {
+    var content = content
+    var writer = writer
+
+//    Ex. 사용자 - 567입력 : ChatData("567","user")
+}
