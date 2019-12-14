5a2f971152b6d34272e07d2b9cdf48afdeb1a17d
gyucode <gyucode@gmail.com>
2019-12-14 15:19:01 +0900

    채팅데이터/ 한줄짜리 XML그리기

--- app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191214_02_baseballgame/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjeit.a20191214_02_baseballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BaseActivity() {
 
@@ -22,7 +23,32 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        makeComputerNumbers()
     }
 
+    fun makeComputerNumbers(){
+        for( i in 0..2){
+            while (true){
+                val randomNum = (Math.random() * 9 + 1).toInt()  // 0 <= 랜덤값 *9< 1
+
+                var isDuplicateOk = true
+
+                for (num in computerNumArray){
+                    if(num == randomNum){
+                        isDuplicateOk = false
+                        break
+                    }
+                }
+
+                if(isDuplicateOk){
+                    computerNumArray.add(randomNum)
+                    break
+                }
+
+            }
+        }
+        for(num in computerNumArray){
+            Log.d("출제된 숫자","${num}")
+        }
+    }
 }
